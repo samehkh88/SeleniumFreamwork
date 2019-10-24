7b0d4cbb57b26c9e473b0f1128c7fc813656d457
sameh khalil <sameh@netservex.com>
2019-10-24 15:38:28 +0200

    add new class for product detalois page

---
+++ src/main/java/pages/ProductdetalisPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class ProductdetalisPage extends PageBase{
+
+	public ProductdetalisPage(WebDriver driver) {
+		super(driver);
+	}
+
+}
